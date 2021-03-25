6cdf515fff7c6e370ebc55227d372ecd7c328ca1
Ashley Kohl <akohl22@gmail.com>
2021-03-24 23:37:24 -0600

    updated 5 day forecast in java

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,7 @@
 var weatherContainer = document.getElementById('weather');
 var historyContainer = document.getElementById('history');
 var searchButton = document.getElementById('search-button');
+var fiveDayContainer = document.getElementById('five-day-container');
 
 
 // set up the API key
@@ -20,6 +21,8 @@ function getApi() {
     })
     .then(function (data) {
       console.log(data);
+      $('#search-value').val('');
+
       var temp = document.createElement('span');
       temp.textContent = "Temperature: " + data.main.temp + " F";
       temp.classList = "list-group";
@@ -28,13 +31,14 @@ function getApi() {
       cityNameEL.textContent = data.name;
 
       var humidity = document.createElement('span');
-      humidity.textContent = "Humidity: " + data.main.humidity;
+      humidity.textContent = "Humidity: " + data.main.humidity + ' %';
       humidity.classList = "list-group";
 
       var windSpeed = document.createElement('span');
       windSpeed.textContent = "Wind Speed: " + data.wind.speed + " mph";
       windSpeed.classList = "list-group";
 
+      weatherContainer.innerHTML = '';
       weatherContainer.append(cityNameEL, temp, humidity, windSpeed);
 
 
@@ -47,17 +51,27 @@ function getApi() {
 
       var lon = data.coord.lon;
       var lat = data.coord.lat;
+
       getUVIndex(lat, lon);
 
-      
+
+      var currentDate = document.createElement("span")
+      currentDate.textContent = " (" + moment(weather.dt).format("MMM D, YYYY") + ") ";
+      cityNameEL.append(currentDate);
+
+
       var weatherIcon = document.createElement("img")
       weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
       cityNameEL.appendChild(weatherIcon);
 
 
+      // getFiveDay(searchValue);
 
     });
 }
+
+
+
 
 function getUVIndex(lat, lon) {
   var queryURL = `https://api.openweathermap.org/data/2.5/uvi?appid=${APIkey}&lat=${lat}&lon=${lon}`;
@@ -72,27 +86,58 @@ function getUVIndex(lat, lon) {
       console.log(data.value);
       UVIndex.classList = "list-group"
 
+      weatherContainer.appendChild(UVIndex);
+
 
     });
 }
 
-// // function getFiveDay(searchValue) {
-// //   var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${searchValue}&appid=${APIkey}`;
-// //   fetch(queryURL)
-// //   .then(function (response) {
-// //     return response.json();
-    
-// //   })
-// //   .then(function (data) {
-// //     console.log(data);
-// //     var fiveDay = document.createElement('span');
-// //     fiveDay.textContent = "Five Day: " + data.value;
-// //     console.log(data);
-// //     fiveDay.classList = "list-group"
 
-// //     fiveDayContainer.append(fiveDay);
-// //   })
-// }
+function getFiveDay() {
+  var searchValue = document.getElementById('search-value').value;
+  var fiveDayUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${searchValue}&units=imperial&appid=${APIkey}`;
+
+  fetch(fiveDayUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+
+      var temp = document.createElement('span');
+      temp.textContent = "Temperature: " + data.list[0].main.temp + ' F';
+      temp.classList = "list-group";
+
+      // var weatherIcon = document.createElement("img")
+      // weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${data.list[0].weather.icon}@2x.png`);
+      // citNameEL.append(weatherIcon);
+
+      
+
+      var currentDate = document.createElement("span")
+      currentDate.textContent = " (" + moment(weather.dt).format("MMM D, YYYY") + ") ";
+      
+
+      var humidity = document.createElement('span');
+      humidity.textContent = "Humidity: " + data.list[0].main.humidity + ' %';
+      humidity.classList = "list-group";
+
+      fiveDayContainer.innerHTML = "";
+      fiveDayContainer.append(currentDate, temp, humidity);
+
+
+     
+      
+    })
+
+
+     
+}
 
 
 searchButton.addEventListener('click', getApi);
+searchButton.addEventListener('click', getFiveDay);
+
+window.addEventListener("load", function () {
+  window.localStorage.getItem("cityHistory");
+})
